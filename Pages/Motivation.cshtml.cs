baba73945b20ebc10c210e88bbdfe15f307ee2f2
Ak47u1999 <arpittopper.2017@gmail.com>
2021-09-02 17:57:10 +0530

    .

---
+++ Pages/Motivation.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CLDTraining2.Pages
+{
+    public class MotivationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
